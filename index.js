35cf1f57bfaded4689f5f7464e03d9dd0721c6b4
eustace douglas <eadouglas@live.co.uk>
2018-10-15 11:19:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function happyHolidayTo(holiday,name){
   return `Happy ${holiday}, ${name}!`
   
 }
-var days = 20;
+//var days = 20;
 function holidayCountdown(holiday,days){
   return `It's ${days} days until ${holiday}!`
 }
